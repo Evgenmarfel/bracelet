a1dca1deb2a5c6d44969a8c7b89852d5cee7f354
Evgen <mail@gmail.com>
2019-12-23 17:40:48 +0200

    333

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-console.log('234') 
+/* let answer = confirm("Вам есть 18?");
+if (answer) {
+    console.log("go")
+} else {
+    console.log("fuck")
+} */
+
+/* for (let i = 1; i < 8; i++ ) {
+    console.log(i);
+} */
+
+/* function log(a, b) {
+    console.log( a + b);
+}
+
+log(7, 14);
+
+log(1, 14); */
