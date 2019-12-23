7aa2a488a4b7471bd414e2d6c49436698aaea096
Evgen <mail@gmail.com>
2019-12-23 20:10:39 +0200

    add slider

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+    $('.courusel__inner').slick({
+        speed: 300,
+        slidesToShow: 1,        
+        autoplay: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icon/chevron-left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icon/chevron-right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false
+                  } 
+            }
+        ]   
+    });
+  });
