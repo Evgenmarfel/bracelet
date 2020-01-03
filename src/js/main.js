cd9d1ccebb10d9a526fc53accb2aefb14cdb6634
Evgen <mail@gmail.com>
2020-01-03 18:38:54 +0200

    add reviews

--- src/js/main.js
+++ src/js/main.js
@@ -10,11 +10,31 @@ $(document).ready(function(){
             {
                 breakpoint: 992,
                 settings: {
-                  dots: true,
-                  arrows: false
+                    arrows: false
                   } 
             }
         ]   
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_activ)', function() {
+        $(this)
+          .addClass('catalog__tab_activ').siblings().removeClass('catalog__tab_activ')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_activ').eq($(this).index()).addClass('catalog__content_activ');
+      });
+
+
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_activ');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_activ');
+            })
+            
+        });
+    };
+    toggleSlide('.catalog-item__linck');
+    toggleSlide('.catalog-item__back');
   });
 
