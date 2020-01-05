58dc72a0404d3a0f62b0fcdba38bc56dc2e96f0b
Evgen <mail@gmail.com>
2020-01-05 19:19:44 +0200

    add modal

--- src/js/main.js
+++ src/js/main.js
@@ -37,5 +37,23 @@ $(document).ready(function(){
     };
     toggleSlide('.catalog-item__linck');
     toggleSlide('.catalog-item__back');
+
+    // modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thancs').fadeOut('slow');
+    });
+   
+
+    $('.button_card').each(function(i) {
+        $(this).on('click', function() {
+            $('#order, .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
   });
 
